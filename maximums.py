e6cc032ea825d195e3609b7f4b128afee4e9478a
juansebas06 <162723503+juansebas06@users.noreply.github.com>
2024-04-26 02:15:08 -0600

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,9 +1,10 @@
 # Replace the "ANSWER HERE" for your answer
 
 def max_of_two(x, y):
-    
-    return max(max_of_two(x, y))
-    
+    if x > y:
+        return x
+    else:
+        return y
 def max_of_three(x, y, z):
    
     return max(max_of_two(x, y), z)
