6dfb1842968de112f582693f5db63bdc5a9eb136
juansebas06 <162723503+juansebas06@users.noreply.github.com>
2024-04-26 02:50:03 -0600

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,17 +1,4 @@
 # Replace the "ANSWER HERE" for your answer
 def number_to_month(month):
-    months = {
-        1: "enero",
-        2: "febrero",
-        3: "marzo",
-        4: "abril",
-        5: "mayo",
-        6: "junio",
-        7: "julio",
-        8: "agosto",
-        9: "septiembre",
-        10: "octubre",
-        11: "noviembre",
-        12: "diciembre"
-    }
+    months = {1: "enero", 2: "febrero", 3: "marzo", 4: "abril", 5: "mayo", 6: "junio", 7: "julio", 8: "agosto", 9: "septiembre", 10: "octubre", 11: "noviembre", 12: "diciembre"}
     return months.get(month, "error")
